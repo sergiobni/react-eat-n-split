f0e06d68ec1c6ce7494dae3d3395ef128d161c29
sergiobni <bni.sergio@gmail.com>
2024-02-06 11:37:06 +0100

    static app completed

--- src/App.js
+++ src/App.js
@@ -18,3 +18,90 @@ const initialFriends = [
     balance: 0,
   },
 ];
+
+export default function App() {
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+        <FormAddFriend />
+        <Button>Add friend</Button>
+      </div>
+      <FormSplitBill />
+    </div>
+  );
+}
+
+function FriendsList() {
+  const friends = initialFriends;
+
+  return (
+    <ul>
+      {friends.map(friend => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 && (
+        <p className="red">You owe {Math.abs(friend.balance)}€</p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">You have {Math.abs(friend.balance)}€</p>
+      )}
+      {friend.balance === 0 && (
+        <p>You owe nothing {Math.abs(friend.balance)}€</p>
+      )}
+      <Button>Select</Button>
+    </li>
+  );
+}
+
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>💷Friend name</label>
+      <input type="text" />
+
+      <label>🎒Image url</label>
+      <input type="text" />
+
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X </h2>
+      <label>🎒Bill value</label>
+      <input type="text" />
+
+      <label>🏝️Your expense</label>
+      <input type="text" />
+
+      <label>🎒X expense </label>
+      <input type="text" disabled />
+
+      <label>🤷‍♂️ Who is playing the bill?</label>
+
+      <select>
+        <option value="user">You</option>
+        <option value="friend">You</option>
+      </select>
+
+      <Button>Add</Button>
+    </form>
+  );
+}
